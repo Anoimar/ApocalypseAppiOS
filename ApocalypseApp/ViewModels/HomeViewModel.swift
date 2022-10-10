c72dfbf73ee9eef98a8e20d61a54e7aa780114b2
mariusz <mariusz.rafalski@norde.com>
2022-10-10 12:34:49 +0200

    removing not used field

--- ApocalypseApp/ViewModels/HomeViewModel.swift
+++ ApocalypseApp/ViewModels/HomeViewModel.swift
@@ -17,9 +17,7 @@ class HomeViewModel: ObservableObject {
         self.marvelService = MarvelService()
         fetchResults()
     }
-    
-    let apiPrimaryKey: String = "9e83a1125c1601e6c450553863acfe1a"
-    
+        
     func fetchResults() {
         self.marvelService.getApocalypseComics(completion: { results in
             if let results = results {
